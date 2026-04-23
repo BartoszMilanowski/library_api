af7d7ea20aad40de9c5a0fe842cfb202e9f2238e
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2026-04-23 20:39:02 +0200

    Create BookResponseDtos

---
+++ src/main/java/com/bartmilan/library_api/dto/BookResponseDto.java
@@ -0,0 +1,67 @@
+package com.bartmilan.library_api.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class BookResponseDto {
+
+    private Long id;
+    private String polishTitle;
+    private String originalTitle;
+    private String description;
+    private LocalDate releaseDate;
+    private String isbn;
+    private List<AuthorToBookResponseDto> authors;
+    private PublisherToBookResponseDto publisher;
+    private String coverUrl;
+
+    public BookResponseDto(Long id, String polishTitle, String originalTitle, String description,
+                           LocalDate releaseDate, String isbn, List<AuthorToBookResponseDto> authors,
+                           PublisherToBookResponseDto publisher, String coverUrl) {
+        this.id = id;
+        this.polishTitle = polishTitle;
+        this.originalTitle = originalTitle;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.isbn = isbn;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.coverUrl = coverUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPolishTitle() {
+        return polishTitle;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public List<AuthorToBookResponseDto> getAuthors() {
+        return authors;
+    }
+
+    public PublisherToBookResponseDto getPublisher() {
+        return publisher;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+}
--- src/main/java/com/bartmilan/library_api/service/BookService.java
+++ src/main/java/com/bartmilan/library_api/service/BookService.java
@@ -1,13 +1,18 @@
 package com.bartmilan.library_api.service;
 
+import com.bartmilan.library_api.dto.AuthorToBookResponseDto;
+import com.bartmilan.library_api.dto.BookResponseDto;
+import com.bartmilan.library_api.dto.PublisherToBookResponseDto;
 import com.bartmilan.library_api.exception.DuplicateResourceException;
 import com.bartmilan.library_api.exception.ResourceNotFoundException;
+import com.bartmilan.library_api.model.Author;
 import com.bartmilan.library_api.model.Book;
 import com.bartmilan.library_api.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -18,38 +23,95 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    public BookResponseDto toDto(Book b) {
+
+        List<Author> authors = b.getAuthors();
+        List<AuthorToBookResponseDto> authorsDto = new ArrayList<>();
+
+        for (Author a : authors) {
+
+            AuthorToBookResponseDto authorDto = new AuthorToBookResponseDto(
+                    a.getId(),
+                    a.getFullName()
+            );
+            authorsDto.add(authorDto);
+        }
+
+        PublisherToBookResponseDto publisherDto = new PublisherToBookResponseDto(
+                b.getPublisher().getId(),
+                b.getPublisher().getName()
+        );
+
+        return new BookResponseDto(
+                b.getId(),
+                b.getPolishTitle(),
+                b.getOriginalTitle(),
+                b.getDescription(),
+                b.getReleaseDate(),
+                b.getIsbn(),
+                authorsDto,
+                publisherDto,
+                b.getCoverUrl()
+        );
+    }
+
     public Book getById(Long id) {
         return bookRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
     }
 
-    public List<Book> getAll() {
-        return bookRepository.findAll();
+    public BookResponseDto getBookDtoById(Long id) {
+        return bookRepository.findById(id)
+                .map(this::toDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
     }
 
-    public List<Book> getByTitle(String title) {
-        return bookRepository.searchByTitle(title);
+    public List<BookResponseDto> getAll() {
+        return bookRepository.findAll()
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
-    public Book getByIsbn(String isbn) {
+    public List<BookResponseDto> getByTitle(String title) {
+        return bookRepository.searchByTitle(title)
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    public BookResponseDto getByIsbn(String isbn) {
         return bookRepository.findByIsbn(isbn)
+                .map(this::toDto)
                 .orElseThrow(() -> new ResourceNotFoundException("Book not found with isbn: " + isbn));
     }
 
-    public List<Book> getByAuthorId(Long authorId) {
-        return bookRepository.findByAuthors_Id(authorId);
+    public List<BookResponseDto> getByAuthorId(Long authorId) {
+        return bookRepository.findByAuthors_Id(authorId)
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
-    public List<Book> getByAuthorsName(String authorName) {
-        return bookRepository.searchByAuthorsName(authorName);
+    public List<BookResponseDto> getByAuthorsName(String authorName) {
+        return bookRepository.searchByAuthorsName(authorName)
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
-    public List<Book> getByPublisherId(Long publisherId) {
-        return bookRepository.findByPublisher_Id(publisherId);
+    public List<BookResponseDto> getByPublisherId(Long publisherId) {
+        return bookRepository.findByPublisher_Id(publisherId)
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
-    public List<Book> getByPublisherName(String publisherName) {
-        return bookRepository.findByPublisher_Name(publisherName);
+    public List<BookResponseDto> getByPublisherName(String publisherName) {
+        return bookRepository.findByPublisher_Name(publisherName)
+                .stream()
+                .map(this::toDto)
+                .toList();
     }
 
     public Book create(Book book) {
